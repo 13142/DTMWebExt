52bc51173f6d46f36a9d1aa54f2f633cae624dcd
Rex Pan <13142@hillcrest-high.school.nz>
2017-05-29 09:43:04 +1200

    Added word expansion

--- ContentScriptsF/CsMain.js
+++ ContentScriptsF/CsMain.js
@@ -3,21 +3,7 @@
 // document.addEventListener('focusin', FocusChange);
 
 var allWordlistData = [];
-var qwertyKeyboardArray = [
-  ['`', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '-', '='],
-  ['', 'q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', ' ', 'p', '[', ']', '\\'],
-  ['', 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', ' ', ';', '\''],
-  ['', 'z', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '/'],
-  ['', '', '', ' ', ' ', ' ', ' ', ' ', '', '']
-];
 
-var qwertyShiftedKeyboardArray = [
-  ['~', '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '+'],
-  ['', 'Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', '', 'P', '{', '}', '|'],
-  ['', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', '', ':', '"'],
-  ['', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?'],
-  ['', '', ' ', ' ', ' ', ' ', ' ', '', '']
-];
 
 window.addEventListener("click", notifyExtension);
 
@@ -30,13 +16,27 @@ function notifyExtension(e) {
 }
 
 function handleResponse(message) {
-  console.log('background script sent a response: ' + message.response[50]);
+  document.activeElement.innerText = message;
+}
+
+function handleSpacedResponse(message) {
+  //console.log(message);
+  //message = JSON.parse(message);
+  var newWord = message.pop();
+  message = message.join(' ');
+  var pattern = "^" + message + "\s?[a-zA-Z]+";
+  var re = new RegExp(pattern, "g");
+  var temp = savedRange.endOffset;
+  document.activeElement.innerText = document.activeElement.innerText.replace(re, message + ' ' + newWord);
+  document.activeElement.innerHTML += "&nbsp;";
+  savedRange.setEnd(document.activeElement, temp);
+  savedRange.collapse(false);
+  restoreSelection();
 }
 
 function handleError(error) {
   console.log('Error: ' + error);
 }
-
 // var stdin = process.openStdin();
 // console.log("ready: ");
 // stdin.addListener("data", function(d) {
@@ -53,25 +53,169 @@ function isEmptyOrSpaces(str) {
 }
 
 function keyDown(zEvent) {
-  console.log(zEvent.code);
   if (zEvent.ctrlKey && zEvent.altKey && zEvent.code === "KeyM") {
     var innerString = document.activeElement.innerText;
-    var splitString = innerString.split(/\s+/);
-    for (var i = 0; i < splitString.length; i++) {
-      if (isEmptyOrSpaces(splitString[i])) {
-        splitString.splice(i,1);
-        i--;
-        continue;
-      }
-      splitString[i] = splitString[i].replace(/[\.\;\,\?\!]+/, "");
-    }
+    var selectedText;
+    // if (selectedText == '') {
+    //   var frames = window.frames;
+    //   for (var i = 0; i < frames.length; i++) {
+    //     if (selectedText == '') {
+    //       selectedText = frames[i].document.getSelection().getRangeAt(0);
+    //       console.log("Yes  " + frames[i].document.getSelection().getRangeAt(0));
+    //     } else {
+    //       break;
+    //     }
+    //   }
+    // }
+    console.log(selectedText);
     var sending = browser.runtime.sendMessage({
-      "textBoxData": splitString
+      "textBoxData": innerString,
+      "requestType": "fullCorrection"
     });
     sending.then(handleResponse);
+  } else if (zEvent.code === "Space") {
+    console.log(doGetCaretPosition(document.activeElement));
+    saveSelection();
+    var sending = browser.runtime.sendMessage({
+      "textBoxData": getWordPrecedingCaret(document.activeElement),
+    //  "textboxPosition": doGetCaretPosition(document.activeElement),
+      "requestType": "spaceCorrection"
+    });
+
+    //  var latestWord =
+    // var sending = browser.runtime.sendMessage({
+    //   "textBoxData": latestWord,
+    //   "requestType": "spaceCorrection"
+    // });
+    sending.then(handleSpacedResponse);
+
+  }
+}
+var savedRange,isInFocus;
+function saveSelection() {
+  if (window.getSelection) //non IE Browsers
+  {
+    savedRange = window.getSelection().getRangeAt(0);
+  } else if (document.selection) //IE
+  {
+    savedRange = document.selection.createRange();
   }
 }
 
+function restoreSelection() {
+  isInFocus = true;
+//  document.getElementById("area").focus();
+  if (savedRange != null) {
+    if (window.getSelection) //non IE and there is already a selection
+    {
+      var s = window.getSelection();
+      if (s.rangeCount > 0)
+        s.removeAllRanges();
+      s.addRange(savedRange);
+    } else if (document.createRange) //non IE and no selection
+    {
+      window.getSelection().addRange(savedRange);
+    } else if (document.selection) //IE
+    {
+      savedRange.select();
+    }
+  }
+}
+//this part onwards is only needed if you want to restore selection onclick
+var isInFocus = false;
+
+function onDivBlur() {
+  isInFocus = false;
+}
+
+function cancelEvent(e) {
+  if (isInFocus == false && savedRange != null) {
+    if (e && e.preventDefault) {
+      //alert("FF");
+      e.stopPropagation(); // DOM style (return false doesn't always work in FF)
+      e.preventDefault();
+    } else {
+      window.event.cancelBubble = true; //IE stopPropagation
+    }
+    restoreSelection();
+    return false; // false = IE style
+  }
+}
+
+function doGetCaretPosition(oField) {
+
+  // Initialize
+  var iCaretPos = 0;
+
+  // IE Support
+  if (window.getSelection) {
+
+    // Set focus on the element
+    //  oField.focus();
+    //var temp = window.getSelection();
+    // To get cursor position, get empty selection range
+    var oSel = window.getSelection().getRangeAt(0).cloneRange();
+
+    // Move selection start to 0 position
+    oSel.setStart(oField, 0);
+    //    oSel.moveStart('character', -oField.value.length);
+
+    // The caret position is selection length
+    iCaretPos = oSel.endOffset;
+  }
+
+  // Firefox support
+  else if (oField.selectionStart || oField.selectionStart == '0')
+    iCaretPos = oField.selectionStart;
+
+  // Return results
+  return iCaretPos;
+}
+
+function getWordPrecedingCaret(containerEl) {
+  var precedingChar, sel, range, precedingRange;
+  if (window.getSelection) {
+    sel = window.getSelection();
+    if (sel.rangeCount > 0) {
+      range = sel.getRangeAt(0).cloneRange();
+      range.collapse(true);
+      range.setStart(containerEl, 0);
+      precedingChar = range.toString().trim();
+    }
+  } else if ((sel = document.selection) && sel.type != "Control") {
+    range = sel.createRange();
+    precedingRange = range.duplicate();
+    precedingRange.moveToElementText(containerEl);
+    precedingRange.setEndPoint("EndToStart", range);
+    precedingChar = precedingRange.text.trim();
+  }
+  return precedingChar;
+
+}
+// function ReturnWord(text, caretPos) {
+//   var index = text.indexOf(caretPos);
+//   var preText = text.substring(0, caretPos);
+//   if (preText.indexOf(" ") > 0) {
+//     var words = preText.split(" ");
+//     return words[words.length - 1]; //return last word
+//   } else {
+//     return preText;
+//   }
+// }
+//
+// function GetCaretPosition(ctrl) {
+//   var CaretPos = 0; // IE Support
+//   if (document.selection) {
+//     ctrl.focus();
+//     var Sel = document.selection.createRange();
+//     Sel.moveStart('character', -ctrl.value.length);
+//     CaretPos = Sel.text.length;
+//   }
+//   // Firefox support
+//   else if (ctrl.selectionStart || ctrl.selectionStart == '0')
+//     CaretPos = ctrl.selectionStart;
+//   return (CaretPos);
+// }
 // function keyDown(e) {console.log(e.which);}; // Test
 // function keyUp(e) {console.log(e);}; // Test
 (function checkForNewIframe(doc) {
